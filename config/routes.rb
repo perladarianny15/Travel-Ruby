8a198c982885a6d1fea8a360f44e3c0db2808d28
Perladarianny15 <developer@Wellingtons-MacBook-Pro-2.local>
2020-03-03 12:57:51 -0400

    New Admin Page when user in.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users 
+  devise_for :users
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index"
+  get 'home/admin', :to => 'home#admin'
+
 end
